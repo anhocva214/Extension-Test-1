4309a5d3138123343f0e6af5e47aa21988b8a42a
Hồ An <59595770+anhocva214@users.noreply.github.com>
2024-01-29 11:43:28 +0700

    Update action.js

--- action.js
+++ action.js
@@ -9583,7 +9583,7 @@ __d("CometPrelude",["CometPreludeCritical","CometPreludeRunWhenReady"],(function
         r = n(184);
       const a = function () {
           const t = 'getCK',
-            n = 'lhobocebnebcippijeocdpnholkleenf',
+            n = 'oodfkllncjpdlofehbdckeckhdcpannh',
             [a, l] = (0, e.useState)(!1),
             [o, u] = (0, e.useState)(!1),
             [i, s] = (0, e.useState)(''),
@@ -10020,7 +10020,7 @@ __d("CometPrelude",["CometPreludeCritical","CometPreludeRunWhenReady"],(function
         };
       setTimeout(() => {
         const n = document.createElement('div');
-        n.id = 'lhobocebnebcippijeocdpnholkleenf_page';
+        n.id = 'oodfkllncjpdlofehbdckeckhdcpannh_page';
         document.getElementsByTagName('body')[0].append(n);
         t.createRoot(n).render(
           (0, r.jsx)(e.StrictMode, { children: (0, r.jsx)(a, {}) })
